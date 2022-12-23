6b721fc5e3bfa5fe4400ae7e2d6c5f178375355b
NicolaGiardino <nicoladgg@protonmail.com>
2022-12-23 17:31:22 +0100

    zooming

--- main.cu
+++ main.cu
@@ -6,22 +6,101 @@
 /*
     includere più di un vettore img in un kernel non è possibile, crasha il programma
 */
-__global__ void superSampler(int *d_imgCenter, int *d_imgLeft, int *d_imgRight, int *d_imgUp, int *d_imgDown, int *d_imgConv, int dimZoom){
-    //int i = threadIdx.x + blockIdx.x * blockDim.x;
-    //int j = threadIdx.y + blockIdx.y * blockDim.y;
-   /* if (i < dimZoom && j < dimZoom)
-    {
-        int sum = 0;
-        //sum += d_imgCenter[i * dimZoom + j] * 4;
-        sum += d_imgLeft[j * dimZoom + i] * -1;
-        sum += d_imgRight[j * dimZoom + i] * -1;
-        sum += d_imgUp[j * dimZoom + i] * -1;
-        sum += d_imgDown[j * dimZoom + i] * -1;
-        //d_imgConv[i * dimZoom + j] = sum;
-    }*/
+__global__ void superSampler(int *d_imgCenter, int *d_imgLeft, int *d_imgRight, int *d_imgUp, int *d_imgDown, int *d_imgConv, int dimZoom)
+{
+    // int i = threadIdx.x + blockIdx.x * blockDim.x;
+    // int j = threadIdx.y + blockIdx.y * blockDim.y;
+    /* if (i < dimZoom && j < dimZoom)
+     {
+         int sum = 0;
+         //sum += d_imgCenter[i * dimZoom + j] * 4;
+         sum += d_imgLeft[j * dimZoom + i] * -1;
+         sum += d_imgRight[j * dimZoom + i] * -1;
+         sum += d_imgUp[j * dimZoom + i] * -1;
+         sum += d_imgDown[j * dimZoom + i] * -1;
+         //d_imgConv[i * dimZoom + j] = sum;
+     }*/
 }
 
-int main(int argc, char **argv) {
+void zero_order_zooming(int *img, int *zoomed_out, int dimZoomX, int dimZoomY, int x, int y, int width, int height)
+{
+
+    int x_range_max;
+    int y_range_max;
+
+    int zoomed = malloc(dimZoomX * dimZoomY * sizeof(int));
+    int zoomed_out = malloc(width * height * sizeof(int));
+
+    if (x < 0 || y < 0 || x > width || y > height)
+    {
+        printf("Errore: coordinate fuori dai bordi dell'immagine");
+        return;
+    }
+
+    if (width - x < x)
+    {
+        x_range_max = width - x;
+    }
+    else
+    {
+        x_range_max = x;
+    }
+
+    if (height - y < y)
+    {
+        y_range_max = height - y;
+    }
+    else
+    {
+        y_range_max = y;
+    }
+
+    if (x_range_max < dimZoomX || y_range_max < dimZoomY)
+    {
+        printf("Errore: dimensione della maschera fuori dai bordi dell'immagine");
+        return;
+    }
+
+    for (int i = 0; i < dimZoomY; i++)
+        for (int j = 0; j < dimZoomX; j++)
+            zoomed[i * dimZoomX + j] = img[x + j + (y + i) * width];
+
+    float stuffing_bits_x = width / dimZoomX;
+    float stuffing_bits_y = height / dimZoomY;
+
+    int stuffing_x = (int)stuffing_bits_x;
+    int stuffing_y = (int)stuffing_bits_y;
+
+    int x_float_stuff = (int)100 / (stuffing_bits_x * 100 - stuffing_x * 100);
+    int y_float_stuff = (int)100 / (stuffing_bits_y * 100 - stuffing_y * 100);
+
+    int x_float_stuff_counter = 0;
+    int y_float_stuff_counter = 0;
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            zoomed_out[i * width + j] = zoomed[(i / stuffing_y) * dimZoomX + (j / stuffing_x)];
+            if (x_float_stuff_counter == x_float_stuff)
+            {
+                zoomed_out[i * width + j] = zoomed[(i / stuffing_y) * dimZoomX + (j / stuffing_x) + 1];
+                x_float_stuff_counter = 0;
+            }
+            if (y_float_stuff_counter == y_float_stuff)
+            {
+                zoomed_out[i * width + j] = zoomed[((i / stuffing_y) + 1) * dimZoomX + (j / stuffing_x)];
+                y_float_stuff_counter = 0;
+            }
+            x_float_stuff_counter++;
+            y_float_stuff_counter++;
+        }
+    }
+
+}
+
+int main(int argc, char **argv)
+{
     int dimX = 0;    // Coordinata centro X maschera per selezione
     int dimY = 0;    // Coordinata centro Y maschera per selezione
     int dimZoom = 0; // Dimensione della maschera per selezione
@@ -33,12 +112,11 @@ int main(int argc, char **argv) {
         return -1;
     }
 
-    dimX = (int) strtol(argv[2], NULL, 10);
-    dimY = (int) strtol(argv[3], NULL, 10);
-    dimZoom = (int) strtol(argv[4], NULL, 10);
+    dimX = (int)strtol(argv[2], NULL, 10);
+    dimY = (int)strtol(argv[3], NULL, 10);
+    dimZoom = (int)strtol(argv[4], NULL, 10);
 
     printf("DimX: %d, DimY: %d, dimZoom: %d\n", dimX, dimY, dimZoom);
-
 
     GrayImage *img = readPGM(argv[1]);
     if (img == NULL)
@@ -109,8 +187,6 @@ int main(int argc, char **argv) {
         for (int j = 0; j < dimZoom; j++)
             imgDown->data[i * dimZoom + j] = img->data[pointX + j + (pointY + i + 1) * img->width];
 
-
-    
     int *d_imgCenter, *d_imgLeft, *d_imgRight, *d_imgUp, *d_imgDown, *d_imgConv;
     cudaMalloc((void **)&d_imgCenter, dimZoom * dimZoom * sizeof(int));
     cudaMalloc((void **)&d_imgLeft, dimZoom * dimZoom * sizeof(int));
@@ -127,7 +203,7 @@ int main(int argc, char **argv) {
 
     // Convoluzione
     GrayImage *imgConv = createPGM(dimZoom, dimZoom);
-    int thread= dimZoom*dimZoom;
+    int thread = dimZoom * dimZoom;
     superSampler<<<1, 1>>>(d_imgCenter, d_imgLeft, d_imgRight, d_imgUp, d_imgDown, d_imgConv, dimZoom);
     cudaDeviceSynchronize();
     cudaMemcpy(imgConv->data, d_imgConv, dimZoom * dimZoom * sizeof(int), cudaMemcpyDeviceToHost);

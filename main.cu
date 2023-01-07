0987160c53ddd3a5513f5f3210acfe99974269c2
NicolaGiardino <nicoladgg@protonmail.com>
2023-01-07 11:39:11 +0100

    convolution + GPU funcs

--- main.cu
+++ main.cu
@@ -1,7 +1,7 @@
 #include "imglib/img.h"
 #include <stdio.h>
 #include <stdlib.h>
-//#include <helper_cuda.h>
+// #include <helper_cuda.h>
 #include <string>
 
 #define N 9
@@ -9,132 +9,89 @@
 signed char sharpness[N] = {0, -1, 0, -1, 4, -1, 0, -1, 0};
 __constant__ float mask[N];
 
-__global__ void resizer(char *start,char *output, int dim_lato_start, int dim_lato_output, int dim_lato_mask, int dim_tot_output){
-
-    const int idx = blockIdx.x * blockDim.x + threadIdx.x;
-    if(idx >= dim_tot_output)
-        return;
-
+void convCPU(char *input, char *output, char *kernel, const int width, const int heigth, const int dimKernel)
+{
+    for (int i = 0; i < width; i++)
+    {
+        for (int j = 0; j < heigth; j++)
+        {
+            int sum = 0;
+            for (int k = 0; k < dimKernel; k++)
+            {
+                for (int l = 0; l < dimKernel; l++)
+                {
+                    sum += input[(i + k) + (j + l) * width] * kernel[k * dimKernel + l];
+                }
+            }
+            output[i + j * width] = sum;
+        }
+    }
 }
 
-/*
-__global__ void convGPU(char *input, char *output, const int dimS, const int dimB)
+__global__ void convGPU(char *input, char *output, char *kernel, const int width, const int heigth, const int dimKernel)
 {
     const int idx = blockIdx.x * blockDim.x + threadIdx.x;
-    if(idx >= dimB)
+    if (idx >= width)
+        return;
+    const int idy = blockIdx.y * blockDim.y + threadIdx.y;
+    if (idy >= heigth)
         return;
 
-    //output[idx] = input[idx] * mask [0] + input[idx + 1] * mask [1] + input[idx + 2] * mask [2] + input[idx + dim] * mask [3] + input[idx + dim + 1] * mask [4] + input[idx + dim + 2] * mask [5] + input[idx + dim*2] * mask [6] + input[idx + dim*2 + 1] * mask [7] + input[idx + dim*2 + 2] * mask [8];
+    int sum = 0;
+    for (int i = 0; i < dimKernel; i++)
+    {
+        for (int j = 0; j < dimKernel; j++)
+        {
+            sum += input[(idx + i) + (idy + j) * width] * kernel[i * dimKernel + j];
+        }
+    }
+    output[idx + idy * width] = sum;
 }
-*/
 
-float* gaussianKernel(const int gaussLength, const float gaussSigma){
-    const char dimension = 3;
-    float *gaussKernel = (float *) malloc(dimension * dimension * sizeof(float));
+void gaussianKernelCPU(const int gaussLength, const float gaussSigma, const char dimension, float *kernel)
+{
     float sum = 0;
-    for(int i = 0; i < dimension; i++){
-        for(int j = 0; j < dimension; j++){
-            gaussKernel[i * dimension + j] = exp(-((i - gaussLength / 2) * (i - gaussLength / 2) + (j - gaussLength / 2) * (j - gaussLength / 2)) / (2 * gaussSigma * gaussSigma));
-            sum += gaussKernel[i * dimension + j];
+    for (int i = 0; i < dimension; i++)
+    {
+        for (int j = 0; j < dimension; j++)
+        {
+            kernel[i * dimension + j] = exp(-((i - gaussLength / 2) * (i - gaussLength / 2) + (j - gaussLength / 2) * (j - gaussLength / 2)) / (2 * gaussSigma * gaussSigma));
+            sum += kernel[i * dimension + j];
         }
     }
-    for(int i = 0; i < dimension; i++){
-        for(int j = 0; j < dimension; j++){
-            gaussKernel[i * dimension + j] /= sum;
+    for (int i = 0; i < dimension; i++)
+    {
+        for (int j = 0; j < dimension; j++)
+        {
+            kernel[i * dimension + j] /= sum;
         }
     }
-    return gaussKernel;
 }
 
-__global__ void zero_order_zooming_GPU(int *img, int *zoomed_out, int dimZoomX, int dimZoomY, int x, int y, int width, int height)
+__global__ void gaussianKernelGPU(const int gaussLength, const float gaussSigma, const char dimension, float *kernel)
 {
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
-    if(idx >= width)
+    if (idx >= dimension * dimension)
         return;
-    int idy = blockIdx.y * blockDim.y + threadIdx.y;
-    if(idy >= height)
-        return;
-    
-    int x_range_max;
-    int y_range_max;
 
-    int *zoomed;
-    cudaMalloc(&zoomed, dimZoomX * dimZoomY * sizeof(int));
+    float sum = 0;
 
-    if (x < 0 || y < 0 || x > width || y > height)
-    {
-        printf("Errore: coordinate fuori dai bordi dell'immagine");
-        return;
-    }
-
-    if (width - x < x)
-    {
-        x_range_max = width - x;
-    }
-    else
-    {
-        x_range_max = x;
-    }
-
-    if (height - y < y)
-    {
-        y_range_max = height - y;
-    }
-    else
-    {
-        y_range_max = y;
-    }
-
-    if (x_range_max < dimZoomX || y_range_max < dimZoomY)
-    {
-        printf("Errore: dimensione della maschera fuori dai bordi dell'immagine");
-        return;
-    }
-
-    if(idx < dimZoomX && idy < dimZoomY)
-        zoomed[idy * dimZoomX + idx] = img[x + idx + (y + idy) * width];
-
-    float stuffing_bits_x = width / dimZoomX;
-    float stuffing_bits_y = height / dimZoomY;
-
-    int stuffing_x = (int)stuffing_bits_x;
-    int stuffing_y = (int)stuffing_bits_y;
-
-    int x_float_stuff = (int)100 / (stuffing_bits_x * 100 - stuffing_x * 100);
-    int y_float_stuff = (int)100 / (stuffing_bits_y * 100 - stuffing_y * 100);
-
-    int x_float_stuff_counter = 0;
-    int y_float_stuff_counter = 0;
+    kernel[idx] = exp(-((idx / dimension - gaussLength / 2) * (idx / dimension - gaussLength / 2) + (idx % dimension - gaussLength / 2) * (idx % dimension - gaussLength / 2)) / (2 * gaussSigma * gaussSigma));
+    sum += kernel[idx];
 
     __syncthreads();
 
-    if(idx < dimZoomX && idy < dimZoomY)
-    {
-        zoomed_out[idx * width + idy] = zoomed[(idx / stuffing_y) * dimZoomX + (idy / stuffing_x)];
-        if (x_float_stuff_counter == x_float_stuff)
-        {
-            zoomed_out[idx * width + idy] = zoomed[(idx / stuffing_y) * dimZoomX + (idy / stuffing_x) + 1];
-            x_float_stuff_counter = 0;
-        }
-        if (y_float_stuff_counter == y_float_stuff)
-        {
-            zoomed_out[idx * width + idy] = zoomed[((idx / stuffing_y) + 1) * dimZoomX + (idy / stuffing_x)];
-            y_float_stuff_counter = 0;
-        }
-        x_float_stuff_counter++;
-        y_float_stuff_counter++;
-    }
-
+    kernel[idx] /= sum;
 }
 
-void zero_order_zooming(int *img, int *zoomed_out, int dimZoomX, int dimZoomY, int x, int y, int width, int height)
+void zero_order_zoomingCPU(int *img, int *zoomed_out, int dimZoomX, int dimZoomY, int x, int y, int width, int height)
 {
 
     int x_range_max;
     int y_range_max;
 
-    int *zoomed = (int *) malloc(dimZoomX * dimZoomY * sizeof(int));
-    zoomed_out = (int *) malloc(width * height * sizeof(int));
+    int *zoomed = (int *)malloc(dimZoomX * dimZoomY * sizeof(int));
+    zoomed_out = (int *)malloc(width * height * sizeof(int));
 
     if (x < 0 || y < 0 || x > width || y > height)
     {
@@ -203,6 +160,86 @@ void zero_order_zooming(int *img, int *zoomed_out, int dimZoomX, int dimZoomY, i
     }
 }
 
+__global__ void zero_order_zoomingGPU(int *img, int *zoomed_out, int dimZoomX, int dimZoomY, int x, int y, int width, int height)
+{
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    if (idx >= width)
+        return;
+    int idy = blockIdx.y * blockDim.y + threadIdx.y;
+    if (idy >= height)
+        return;
+
+    int x_range_max;
+    int y_range_max;
+
+    int *zoomed;
+    cudaMalloc(&zoomed, dimZoomX * dimZoomY * sizeof(int));
+
+    if (x < 0 || y < 0 || x > width || y > height)
+    {
+        printf("Errore: coordinate fuori dai bordi dell'immagine");
+        return;
+    }
+
+    if (width - x < x)
+    {
+        x_range_max = width - x;
+    }
+    else
+    {
+        x_range_max = x;
+    }
+
+    if (height - y < y)
+    {
+        y_range_max = height - y;
+    }
+    else
+    {
+        y_range_max = y;
+    }
+
+    if (x_range_max < dimZoomX || y_range_max < dimZoomY)
+    {
+        printf("Errore: dimensione della maschera fuori dai bordi dell'immagine");
+        return;
+    }
+
+    if (idx < dimZoomX && idy < dimZoomY)
+        zoomed[idy * dimZoomX + idx] = img[x + idx + (y + idy) * width];
+
+    float stuffing_bits_x = width / dimZoomX;
+    float stuffing_bits_y = height / dimZoomY;
+
+    int stuffing_x = (int)stuffing_bits_x;
+    int stuffing_y = (int)stuffing_bits_y;
+
+    int x_float_stuff = (int)100 / (stuffing_bits_x * 100 - stuffing_x * 100);
+    int y_float_stuff = (int)100 / (stuffing_bits_y * 100 - stuffing_y * 100);
+
+    int x_float_stuff_counter = 0;
+    int y_float_stuff_counter = 0;
+
+    __syncthreads();
+
+    if (idx < dimZoomX && idy < dimZoomY)
+    {
+        zoomed_out[idx * width + idy] = zoomed[(idx / stuffing_y) * dimZoomX + (idy / stuffing_x)];
+        if (x_float_stuff_counter == x_float_stuff)
+        {
+            zoomed_out[idx * width + idy] = zoomed[(idx / stuffing_y) * dimZoomX + (idy / stuffing_x) + 1];
+            x_float_stuff_counter = 0;
+        }
+        if (y_float_stuff_counter == y_float_stuff)
+        {
+            zoomed_out[idx * width + idy] = zoomed[((idx / stuffing_y) + 1) * dimZoomX + (idy / stuffing_x)];
+            y_float_stuff_counter = 0;
+        }
+        x_float_stuff_counter++;
+        y_float_stuff_counter++;
+    }
+}
+
 int main(int argc, char **argv)
 {
     // Filter Setup
@@ -223,7 +260,8 @@ int main(int argc, char **argv)
         break;
 
     // Sharpness or Custom Kernel
-    case 6:{
+    case 6:
+    {
         int mode = (int)strtol(argv[5], NULL, 10);
         if (mode == 0)
             cudaMemcpyToSymbol(mask, sharpness, N * sizeof(char));
@@ -256,10 +294,11 @@ int main(int argc, char **argv)
             return -1;
         }
     }
-        break;
+    break;
 
     // Gaussian
-    case 8:{
+    case 8:
+    {
         int mode = (int)strtol(argv[5], NULL, 10);
         if (mode != 1)
         {
@@ -278,18 +317,17 @@ int main(int argc, char **argv)
         cudaMemcpyToSymbol(mask, gaussKernel, N * sizeof(float));
         free(gaussKernel);
     }
-        break;
-    
+    break;
+
     default:
         printf("Wrong command line input. Use -h or --help for more information\n");
         return -1;
     }
 
-    //fine switch case
-    const int dimX = (int)strtol(argv[2], NULL, 10);        // X coordinate of the center of the selection zone
-    const int dimY = (int)strtol(argv[3], NULL, 10);        // Y coordinate of the center of the selection zone
-    const int dimZoom = (int)strtol(argv[4], NULL, 10);     // Length of the side of the selection mask which has a square shape
-
+    // fine switch case
+    const int dimX = (int)strtol(argv[2], NULL, 10);    // X coordinate of the center of the selection zone
+    const int dimY = (int)strtol(argv[3], NULL, 10);    // Y coordinate of the center of the selection zone
+    const int dimZoom = (int)strtol(argv[4], NULL, 10); // Length of the side of the selection mask which has a square shape
 
     // check dimZoom is even
     if (dimZoom % 2 != 0)
@@ -301,7 +339,7 @@ int main(int argc, char **argv)
     printf("DimX: %d, DimY: %d, dimZoom: %d\n", dimX, dimY, dimZoom);
 
     // Check input file ends with .ppm
-    if(std::string(argv[1]).size() < 4 || std::string(argv[1]).substr(std::string(argv[1]).size() - 4) != ".ppm")
+    if (std::string(argv[1]).size() < 4 || std::string(argv[1]).substr(std::string(argv[1]).size() - 4) != ".ppm")
     {
         printf("Error: input file must be a .ppm file\n");
         return -1;
@@ -333,13 +371,13 @@ int main(int argc, char **argv)
         printf("Error: X mask outside image boundaries");
         return -1;
     }
-    
-    // Selection    
+
+    // Selection
     const int inConvDim = dimZoom + 2;
-    const int outScaleDim = (img->width >= img->height) ? img->width : img->height;    
+    const int outScaleDim = (img->width >= img->height) ? img->width : img->height;
     const int pxCount = outScaleDim * outScaleDim * 3;
     RGBImage *imgScaled = createPPM(outScaleDim, outScaleDim);
-    unsigned char *startingMatrix = (unsigned char *)malloc(inConvDim * inConvDim * 3 * sizeof(unsigned char));    
+    unsigned char *startingMatrix = (unsigned char *)malloc(inConvDim * inConvDim * 3 * sizeof(unsigned char));
 
     for (int i = 0; i < dimZoom; i++)
         for (int j = 0; j < dimZoom; j++)
@@ -359,7 +397,8 @@ int main(int argc, char **argv)
     // Check GPU
     int nDevices;
     cudaError_t err = cudaGetDeviceCount(&nDevices);
-    if (err != cudaSuccess) {
+    if (err != cudaSuccess)
+    {
         printf("%s\n", cudaGetErrorString(err));
         return -1;
     }
@@ -367,17 +406,18 @@ int main(int argc, char **argv)
     cudaDeviceProp prop;
     cudaGetDeviceProperties(&prop, 0);
     const int maxThreads = prop.maxThreadsPerBlock;
-    //calcolo numero blocchi necessari 
+    // calcolo numero blocchi necessari
     const int blockCeiling = (dimZoom * 3 * dimZoom / maxThreads) + 1;
-    //controllo numero blocchi utilizzabili
-    if (blockCeiling > prop.maxGridSize[0]){
+    // controllo numero blocchi utilizzabili
+    if (blockCeiling > prop.maxGridSize[0])
+    {
         printf("%s\n", cudaGetErrorString(err));
         return -1;
     }
 
-    //chiamata kernel:passiamo ritaglio iniziale, output, dim ritaglio iniziale, dim output, dim maschera, numero pixel output (non passiamo maschera perchè è già in memoria costante)
-    resizer<<<blockCeiling, maxThreads>>>(d_start,d_scale, inConvDim, outScaleDim, dimZoom, pxCount);
-    //convGPU<<<blockCeiling, maxThreads>>>(d_start, d_scale, dimZoom*dimZoom*3, pxCount);
+    // chiamata kernel:passiamo ritaglio iniziale, output, dim ritaglio iniziale, dim output, dim maschera, numero pixel output (non passiamo maschera perchè è già in memoria costante)
+    resizer<<<blockCeiling, maxThreads>>>(d_start, d_scale, inConvDim, outScaleDim, dimZoom, pxCount);
+    // convGPU<<<blockCeiling, maxThreads>>>(d_start, d_scale, dimZoom*dimZoom*3, pxCount);
     cudaMemcpy(imgScaled->data, d_scale, pxCount * sizeof(char), cudaMemcpyDeviceToHost);
     cudaDeviceSynchronize();
 
